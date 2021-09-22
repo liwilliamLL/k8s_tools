c733671a5163a3ef8d3197accb621fdb41fd3aa5
伟林 <523763256@qq.com>
2021-09-22 17:05:48 +0800

    修改执行脚本，一键安装

--- pullImage.sh
+++ pullImage.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Script For Quick Pull K8S Docker Images
 # by li,william
+# use  centos7.6
+
 
 iptables -F  #清除防火墙规则
 systemctl stop firewalld  #关闭防火墙
@@ -9,6 +11,20 @@ setenforce 0   #关闭selinux
 
 echo "1">/proc/sys/net/bridge/bridge-nf-call-iptables
 echo "1">/proc/sys/net/bridge/bridge-nf-call-ip6tables
+
+mkdir /etc/cni
+mkdir /etc/cni/net.d/
+#cp ./10-flannel.conflist   /etc/cni/net.d/
+cp ./10-calico.conflist    /etc/cni/net.d/
+
+cp ./kubernetes.repo /etc/yum.repos.d/
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+yum install -y docker
+
+systemctl daemon-reload
+systemctl enbale docker && systemctl start docker
+systemctl enbale kubelet &&systemctl start kubelet
+
 
 KUBE_VERSION=v1.22.2    #kubeadm的版本 
 PAUSE_VERSION=3.5     #暂停容器的镜像版本
